e62b2c0a8924ae7da3212611e31358fce5f73384
xfmeng17 <xfmeng17@foxmail.com>
2020-04-22 14:53:56 +0800

    chore(/): init

---
+++ src/sort/quicksort.c
@@ -0,0 +1,26 @@
+#include "quicksort.h"
+
+void swap(int* a, int* b) {
+	int t = *a;
+	*a = *b;
+	*b = t;
+	return;
+}
+
+int quicksort(int* arr, int p, int q) {
+  if (p >= q) {
+    return 0;
+  }
+  int i = p;
+  int j = i + 1;
+  for (; j <= q; j++) {
+    if (arr[j] <= arr[i]) {
+      i++;
+	  swap(&arr[i], &arr[j]);
+    }
+  }
+  swap(&arr[p], &arr[i]);
+  quicksort(arr, p, i-1);
+  quicksort(arr, i+1, q);
+  return 0;
+}
